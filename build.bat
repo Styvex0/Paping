a5289b6aa43741ce5a7042c24118142b330dea91
@Styvex <119627760+Styvex0@users.noreply.github.com>
2024-06-07 23:37:54 +0200

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo Building the required modules...
+
+pip install socket
+pip install time
+pip install colorama
+
+echo Build completed.
+
+echo If you have any errors, please check the error message above and read the python documentation!
+
+@pause
